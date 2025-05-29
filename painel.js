eca5db80ee2492d25a42f93bc8bd7c72b52745ca
getscript13 <gitscripts69@gmail.com>
2025-05-29 19:14:51 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,14 +1,4 @@
-// ==UserScript==
-// @name         Ken AI Painel
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @match        *://*/*
-// @grant        none
-// @require      https://raw.githubusercontent.com/getscript13/ui.js/main/painel.js
-// @require      https://raw.githubusercontent.com/getscript13/ui.js/main/actions.js
-// ==/UserScript==
 // painel.js
-
 (function () {
   window.kenUI = {
     createPainel: function () {
@@ -27,7 +17,4 @@
       document.body.appendChild(div);
     }
   };
-
-  window.kenUI.createPainel();
-  window.kenActions.start();
 })();
